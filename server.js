b4f1652d73d4ce5cc1ae46f1c09ea2b1cb59fcf2
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2019-02-06 15:15:07 -0800

    changed homepage

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 var port = 3000;
 
-app.use(express.static(__dirname + '/public/'));
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'html');
 app.listen(port,()=>{
   console.log("listening at http://127.0.0.1:" + port);

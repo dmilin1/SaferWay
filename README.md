03d900015815ee506a79c1c3d13f75f01082bb60
Dimitrie Milinovich <Dimitrie@MacBookPro-2.local>
2019-02-06 12:57:08 -0800

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+# SaferWay
+
+<img src='https://github.com/dmilin1/SaferWay/blob/master/images/SaferWay.png'/>
---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+var port = 3000;
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'html');
+app.listen(port,()=>{
+  console.log("listening at http://127.0.0.1:" + port);
+});

b83acbadb9d33bcf8946f00b2ea85aa5b8445b3e
Dimitrie Milinovich <Dimitrie@MacBookPro-2.local>
2019-02-07 08:29:56 -0800

    MongoDB added

--- server.js
+++ server.js
@@ -8,3 +8,33 @@ app.set('view engine', 'html');
 app.listen(port,()=>{
   console.log("listening at http://127.0.0.1:" + port);
 });
+
+
+
+// const mongoose = require('mongoose');
+//
+// mongoose.connect('mongodb://localhost/saferway');
+//
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   // we're connected!
+//
+//   var products = new mongoose.Schema({
+//     name: String,
+//     price: Number,
+//     updated: { type: Date, default: Date.now },
+//   });
+//
+//   var Product = mongoose.model('Product', products);
+//
+//   var exampleProduct1 = new Product({
+//     name: "Tomato",
+//     price: 0.99,
+//   })
+//
+//   exampleProduct1.save(function (err, exampleProduct1) {
+//     if (err) return console.error(err);
+//   });
+//
+// });

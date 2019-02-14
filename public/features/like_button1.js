38e8091cec63ee38bb9258cf105ab53e2f84b90c
gregorymayo <gregoryjuevesii23@gmail.com>
2019-02-13 23:11:44 -0800

    react environment

---
+++ public/features/like_button1.js
@@ -0,0 +1,24 @@
+
+'use strict';
+
+class LikeButton1 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return (
+      <button onClick={() => this.setState({ liked: true }) }>
+        Like
+      </button>
+    );
+  }
+}
+
+let domContainer1 = document.querySelector('#like_button_container1');
+ReactDOM.render(<LikeButton />, domContainer1);

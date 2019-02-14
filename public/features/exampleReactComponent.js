ec84c69962b3924d558a9600cc3c7389d75fe7ec
Dimitrie Milinovich <Dimitrie@MacBookPro-2.local>
2019-02-14 08:09:15 -0800

    React Demo for frontend team

---
+++ public/features/exampleReactComponent.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+class Root extends React.Component {
+  constructor(props) {
+    super(props);
+    this.msg = 'THIS IS MY REACT EXAMPLE TEXT' ;
+  }
+
+  render() {
+    console.log(this.exampleProp)
+    return (
+      <div>
+      Example: {this.msg}
+      </div>
+    );
+  }
+}
+
+
+
+ReactDOM.render(<Root/>, document.getElementById('exampleReactComponent'));

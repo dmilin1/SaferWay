015d0da9a3975708d9f301d3678a86cb11313a37
justin-nguyen03 <38301747+justin-nguyen03@users.noreply.github.com>
2019-02-20 12:47:15 -0800

    Header placeholder (not usable yet)

---
+++ public/pages/components/header.js
@@ -0,0 +1,14 @@
+
+export default class Header extends Component {
+	render () {
+
+		return {
+			<div>
+				<a href="#"><img src="/images/1.png" alt="SaferWay" width="200"></a>
+				<ul className="menu-ul">
+					<li><Link to="/">Home</Link></li>
+					
+
+		}
+	}
+}

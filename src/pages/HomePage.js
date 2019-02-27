755f1d8a9e918fa5d9715f51770f4be377e551dc
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2019-02-27 12:21:36 -0800

    homepage links work

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,25 +7,35 @@ export default class HomePage extends Component {
     return (
     <div>
         <div className="container">
+
             <div className="box">
+
                 <div className="icon"><i className="fa fa-user" aria-hidden="true"></i></div>
                 <div className="content">
+                  <a href="/login" to="/login">
                     <h3>Log In</h3>
                     <p>Log In</p>
+                  </a>
                 </div>
             </div>
+
             <div className="box">
                 <div className="icon"><i className="fa fa-shopping-cart" aria-hidden="true"></i></div>
                 <div className="content">
+                  <a href="/checkout" to="/checkout">
                     <h3>Checkout</h3>
                     <p>Checkout</p>
+                  </a>
                 </div>
             </div>
+
             <div className="box">
                 <div className="icon"><i className="fa fa-search" aria-hidden="true"></i></div>
                 <div className="content">
+                  <a href="/#" to="/#">
                     <h3>search</h3>
                     <p>Search</p>
+                  </a>
                 </div>
             </div>
         </div>

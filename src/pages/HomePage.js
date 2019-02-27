00785cb3a13b63b27ac1c196f35128c54ac21190
Dimitrie Milinovich III <dmilinovichiii@gmail.com>
2019-02-27 12:49:15 -0800

    Home page links now clickable

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,38 +3,38 @@ import './HomePage.css';
 import Navbar from './CustomNavbar.js'
 
 export default class HomePage extends Component {
+
+  openLink(link) {
+    console.log(link)
+    window.location.href = link;
+  }
+
   render() {
     return (
     <div>
         <div className="container">
 
-            <div className="box">
+            <div className="box" onClick={() => this.openLink("login")}>
                 <div className="icon"><i className="fa fa-user" aria-hidden="true"></i></div>
                 <div className="content">
-                  <a href="/login" to="/login">
                     <h3>Log In</h3>
                     <p>Log In</p>
-                  </a>
                 </div>
             </div>
 
-            <div className="box">
+            <div className="box" onClick={() => this.openLink("checkout")}>
                 <div className="icon"><i className="fa fa-shopping-cart" aria-hidden="true"></i></div>
                 <div className="content">
-                  <a href="/checkout" to="/checkout">
                     <h3>Checkout</h3>
                     <p>Checkout</p>
-                  </a>
                 </div>
             </div>
 
             <div className="box">
                 <div className="icon"><i className="fa fa-search" aria-hidden="true"></i></div>
                 <div className="content">
-                  <a href="/#" to="/#">
                     <h3>search</h3>
                     <p>Search</p>
-                  </a>
                 </div>
             </div>
         </div>

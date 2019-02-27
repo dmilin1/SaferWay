6bf9125c0550aad0e94797991a46ddfdf54e114f
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2019-02-27 10:58:19 -0800

    added sample products

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,12 +1,56 @@
 import React, { Component } from 'react';
+import './ProductPage.css';
 
 export default class ProductPage extends Component {
 
   render() {
     return (
-      <div>
-        ProductPage
+      <div className="productList">
+        <ul>
+          <li>
+            <ProductComponent
+              imgsrc="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Milk_glass.jpg/220px-Milk_glass.jpg"
+              alt="milk"
+              title="Milk"
+              price="$1.99"
+            />
+          </li>
+          <li>
+            <ProductComponent
+              imgsrc="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Milk_glass.jpg/220px-Milk_glass.jpg"
+              alt="milk"
+              title="Milk"
+              price="$1.99"
+            />
+          </li>
+          <li>
+            <ProductComponent
+              imgsrc="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Milk_glass.jpg/220px-Milk_glass.jpg"
+              alt="milk"
+              title="Milk"
+              price="$1.99"
+            />
+          </li>
+          <li>
+            <ProductComponent
+              imgsrc="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Milk_glass.jpg/220px-Milk_glass.jpg"
+              alt="milk"
+              title="Milk"
+              price="$1.99"
+            />
+          </li>
+        </ul>
       </div>
     );
   }
+}
+
+function ProductComponent(props){
+  return(
+    <div className="individualComponent">
+      <img src={props.imgsrc} alt={props.alt}/>
+      <p>{props.title}</p>
+      <p>{props.price}</p>
+    </div>
+  );
 }

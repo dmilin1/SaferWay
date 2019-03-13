d274c2acbd28e1e46b7ff862379fdeae6a2dd712
Maneek Dhillon <maneekdhillon@Maneeks-MacBook-Pro.local>
2019-03-13 16:24:30 -0700

    Database Setup by Maneek and Gaston Pics added for test items

--- server/server.js
+++ server/server.js
@@ -4,6 +4,19 @@ const pino = require('express-pino-logger')();
 const mongoose = require('mongoose');
 
 const port = 3001;
+String.prototype.hashCode = function() {// gotten from stackoverflow
+    var hash = 0;
+    if (this.length == 0) {
+        return hash;
+    }
+    for (var i = 0; i < this.length; i++) {
+        var char = this.charCodeAt(i);
+        hash = ((hash<<5)-hash)+char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
+
 
 function launchServer() {
   const app = express();
@@ -15,7 +28,7 @@ function launchServer() {
   );
 }
 
-mongoose.connect('mongodb://localhost/saferway');
+mongoose.connect('mongodb://localhost/saferway', { useNewUrlParser: true });
 
 var db = mongoose.connection;
 
@@ -27,4 +40,264 @@ db.on('error', function () {
 db.once('open', function () {
    console.log('Connected to database');
    launchServer()
+});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  //console.log("connected")
+//   // we're connected!
+
+  var products = new mongoose.Schema({
+    name: { type: String, unique: true },
+    price: Number,
+    category: String,
+    aisle: Number,
+    size: String,
+    inventoryCount: Number,
+    picPath: String,
+    updated: { type: Date, default: Date.now },
+  });
+  var Product = mongoose.model('Product', products);
+
+  var login = new mongoose.Schema({
+     firstName: String,
+     lastName: String,
+     password: String,
+     email: { type: String, unique: true },
+     address: String,
+     cartId: Number,
+     cartSize: Number,
+     updated: { type: Date, default: Date.now },
+  });
+  var Login = mongoose.model('Login', login);
+
+  var cart = new mongoose.Schema({
+    name: { type: String, unique: true },
+    price: Number,
+    category: String,
+    aisle: Number,
+    size: String,
+    inventoryCount: Number,
+    picPath: String,
+    updated: { type: Date, default: Date.now },
+  });
+
+  //Working
+  function search(findName, callback) {
+    var query = { name: findName };
+    Product.findOne(query, function(err, result) {
+       if (err) throw err;
+       if (result) {
+         callback("true")
+         return true;
+       }
+       else {
+         callback("false")
+         return false;
+       }
+    });
+  }
+
+  //Working
+  function insertProduct(name, price, category, aisle, size, inventoryCount, picPath) {
+      search(name, runAfterSearch);
+      function runAfterSearch(result) {
+        if (result == "false") {
+          var temp = new Product({
+             name: name,
+             price: price,
+             category: category,
+             aisle: aisle,
+             size: size,
+             inventoryCount: inventoryCount,
+             picPath: picPath,
+          })
+          temp.save(function (err, temp) {
+             if (err) return console.error(err);
+          });
+        }
+        else {
+          console.log("Product already in Database");
+        }
+      }
+  }
+
+  //Working
+  function searchLogin(findEmail, callback) {
+    var query = { email: findEmail };
+    Login.findOne(query, function(err, result) {
+       if (err) throw err;
+       if (result) {
+         callback("true")
+         return true;
+       }
+       else {
+         callback("false")
+         return false;
+       }
+    });
+  }
+
+  //Working
+  function addUser(firstName, lastName, password, email, address) {
+    searchLogin(email, newUser)
+    function newUser(result) {
+      if (result == "false") {
+        var temp = new Login({
+          firstName: firstName,
+          lastName: lastName,
+          password: password,
+          email: email,
+          address: address,
+          cartId: String(email.hashCode()),
+          cartSize: 0,
+        })
+        temp.save(function (err, temp) {
+          if (err) return console.error(err);
+        });
+        var Cart = mongoose.model(String(email.hashCode()), cart);
+      }
+      else {
+        console.log("User already in Database");
+      }
+    }
+  }
+
+  /*
+  //Not working
+  function searchCart(cartNumber, productName, callback) {
+    var query = { name: productName };
+    String(cartNumber).findOne(query, function(err, result) {
+       if (err) throw err;
+       if (result) {
+         callback("true")
+         return true;
+       }
+       else {
+         callback("false")
+         return false;
+       }
+    });
+  }
+
+  //NEXT TIME TRY TO INSERT RESULT FROM SEARCHING FOR AN ITEM
+  //THIS METHOD IS FUCKED UP!!! FIX IT!!!
+  function updateUserCart(userEmail, productName, quantity) {
+    var query = { email: userEmail };
+    var cart1;
+    Login.findOne(query, function(err, result) {
+       if (err) throw err;
+       if (result) {
+         cart1 = result.cartId;
+         var Cart2 = mongoose.model(cart1,cart);
+         //db.cart1.findOne();
+         console.log(cart1);
+         var q2 = { name: productName };
+         db.findOne(cart1, function(err, result2) {
+            if (err) throw err;
+            if (result2) {
+              return true;
+            }
+            else {
+              return false;
+            }
+         });
+         console.log("Cart found");
+         return true;
+       }
+       else {
+         console.log("Cart not found");
+         return false;
+       }
+    });
+  }
+  */
+
+
+
+
+
+  //Working but can be improved
+  function updatePassword(email, newPassword) {
+     Login.findOneAndUpdate({email: email}, {$set:{password: newPassword}}, function(err, doc) {
+       if (err) return console.error(err);
+     })
+  }
+
+  //Working
+  function updatePrice(productName, newPrice) {
+     Product.findOneAndUpdate({name: productName}, {$set:{price: newPrice}}, function(err, doc) {
+       if (err) return console.error(err);
+     })
+  }
+
+  //Working
+  function updateSize(productName, newSize) {
+     Product.findOneAndUpdate({name: productName}, {$set:{size: newSize}}, function(err, doc) {
+       if (err) return console.error(err);
+     })
+  }
+
+  //Working
+  function updateCount(productName, newCount) {
+     Product.findOneAndUpdate({name: productName}, {$set:{inventoryCount: newCount}}, function(err, doc) {
+       if (err) return console.error(err);
+     })
+  }
+
+  //Working
+  function productSold(productName, quantity) {
+    console.log("derp")
+    var count;
+    var query = { name: productName };
+    Product.findOne(query, function(err, result) {
+       if (err) throw err;
+       if (result) {
+         count = result.inventoryCount - quantity;
+         Product.findOneAndUpdate({name: productName}, {$set:{inventoryCount : count}}, function(err, doc) {
+           if (err) return console.error(err);
+         })
+         return true;
+       }
+       else {
+         console.log("Product not found");
+         return false;
+       }
+    });
+  }
+
+  //Working. Use this for Search bar
+  function userSearch(input) {
+    var query = { name: {$regex: '.*' + input + '.*', '$options' : 'i'} };
+    Product.find(query, function(err, result) {
+       if (err) throw err;
+       if (result) {
+         if(result == "") {
+           console.log("No products found");
+         }
+         else {
+           console.log(result);
+         }
+         return true;
+       }
+       else {
+         return false;
+       }
+    });
+  }
+
+  //userSearch("ttt");
+
+  insertProduct("Milk", 2.89, "Dairy", 1, "1 gallon", 100, "/⁨productPics⁩/milk.jpeg");
+  insertProduct("Apple", 0.99, "Fruit", 2, "1", 100, "/⁨productPics⁩/apple.jpeg");
+  insertProduct("Peanut Butter", 3.89, "Spreads", 3, "16 oz", 100, "/⁨productPics⁩/peanutButter.jpeg");
+  insertProduct("Cookies", 4.49, "Snacks", 4, "16", 100, "/⁨productPics⁩/cookies.jpeg");
+  insertProduct("Potato", 0.89, "Vegetable", 2, "1", 100, "/⁨productPics⁩/potato.jpeg");
+  insertProduct("Lettuce", 1.29, "Vegetable", 2, "1", 100, "/⁨productPics⁩/lettuce.jpeg");
+  insertProduct("Eggs", 3.59, "Deli", 1, "12 count", 100, "/⁨productPics⁩/eggs.jpeg");
+
+
+  addUser("Bob", "Jackson", "password", "bob@gmail.com", "321 10th St. Santa Clara, CA 91123");
+  addUser("John", "Smith", "password", "test@yahoo.com", "123 1st St. San Jose, CA 95123");
 });

a05e886ea49fae926911c0c5947944c16eacdc2e
Sukhvir Singh <sukhvir23singh@gmail.com>
2019-04-17 12:19:42 -0700

    connected shopping cart to database

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -14,19 +14,27 @@ export default class LoginPage extends Component {
             <h3 style={{color:"black", padding:"16px 0 38px 0"}}>Benefits of an account:</h3>
               <ul className="list-group" style={{color:"black"}}>
                 <li className="list-group-item">
-                  <h5>Permanent Shopping Cart <i className="fas fa-cart-plus" style={{margin:"0 0 0 8px"}}></i> </h5>
+                  <h5 style={{color:"black", fontSize:"18px"}}>
+                    Permanent Shopping Cart <i className="fas fa-cart-plus" style={{margin:"0 0 0 8px"}}></i>
+                  </h5>
                   <p>Continue shopping from where you left off last time.</p>
                 </li>
                 <li className="list-group-item">
-                  <h5>Shopping History <i className="fas fa-history" style={{margin:"0 0 0 8px"}}></i> </h5>
+                  <h5 style={{color:"black", fontSize:"18px"}}>
+                    Shopping History <i className="fas fa-history" style={{margin:"0 0 0 8px"}}></i>
+                  </h5>
                   <p>View what items you've bought in the past.</p>
                 </li>
                 <li className="list-group-item">
-                  <h5>Faster Checkout <i className="fas fa-shipping-fast" style={{margin:"0 0 0 8px"}}></i> </h5>
+                  <h5 style={{color:"black", fontSize:"18px"}}>
+                    Faster Checkout <i className="fas fa-shipping-fast" style={{margin:"0 0 0 8px"}}></i>
+                  </h5>
                   <p>Checkout your shopping cart faster and more easily.</p>
                 </li>
                 <li className="list-group-item">
-                  <h5>Personal Info <i className="fas fa-user-check" style={{margin:"0 0 0 8px"}}></i></h5>
+                  <h5 style={{color:"black", fontSize:"18px"}}>
+                    Personal Info <i className="fas fa-user-check" style={{margin:"0 0 0 8px"}}></i>
+                  </h5>
                   <p>Never enter your personal shipping info twice.</p>
                 </li>
               </ul>

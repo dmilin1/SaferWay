76391303d658009d4f7cc227d0ce98a741cf12d3
Sukhvir Singh <sukhvir23singh@gmail.com>
2019-04-20 19:56:38 -0700

    added logout ut button to navbar

---
+++ src/pages/components/LogoutButton.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+//import {Link } from 'react-router-dom';
+import axios from 'axios';
+
+class LogoutButton extends Component{
+    constructor() {
+        super();
+    }
+   logout = () =>{
+       var loginState = { 'loggedin': false };
+       localStorage.setItem('loginState', JSON.stringify(loginState));
+       window.location.href='/';
+   }
+    render(){
+        return(
+            <div>
+                <button className="btn btn-danger" style={{margin:"0 0 0 1em"}} onClick={this.logout}>Logout</button>
+            </div>
+        );
+    }
+}
+
+export default LogoutButton;
